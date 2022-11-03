3d076b73f65f503d29d433b4e310674a395ea0a7
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-11-03 23:53:12 +0530

    ADDED: Abiblity to Launch BridgeLabz Site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Hello_World';
   imgUrl="../assets/download.png";
+  url ="https://www.bridgelabz.com";
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz.";
+    }
+    onClick($event: any){
+      console.log("Save button is clicked!", $event);
+      window.open(this.url, "_blank");
     }
 }
